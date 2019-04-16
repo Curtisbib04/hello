8d72ad245bb61f89f8bbd65506d00ac75b2d13be
Curtisbib04 <e.abdykaimova@yandex.com>
2019-04-16 17:09:21 +0600

    Updates

---
+++ precache-manifest.3699cc6d397432eaea691678da83a8bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ded4da0fe71cbdcab24f",
+    "url": "/hello/static/js/runtime~main.f3b3c316.js"
+  },
+  {
+    "revision": "6793bd51c3dc6a7016f4",
+    "url": "/hello/static/js/main.830e1b59.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/hello/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "6793bd51c3dc6a7016f4",
+    "url": "/hello/static/css/main.cad500a1.chunk.css"
+  },
+  {
+    "revision": "2f3b936eaa45e05fa075e3b5e85aa944",
+    "url": "/hello/index.html"
+  }
+];
